89dfab49e9ad430fe346cf02da0d71d0d57193f7
m.sakamaki <m.sakamaki@gxp.co.jp>
2014-10-25 21:03:32 +0900

    add async test

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -29,3 +29,28 @@ console.log(generator.next().value);
 console.log(generator.next().value);
 console.log(generator.next().value);
 //return passed;
+
+
+console.log(' ------ Async Functions(ES7) ------ ');
+function timeout(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+async function asyncValue(value) {
+  console.log('timeout in', new Date());
+  await timeout(2000);
+  console.log('timeout in', new Date());
+  return value;
+}
+
+(async function() {
+  var value = await asyncValue(42).catch(console.error.bind(console));
+  console.log('before', value);
+  timeout(2000).then(() => {
+    console.log('done');
+  });
+  console.log('after', value);
+  done();
+})();
